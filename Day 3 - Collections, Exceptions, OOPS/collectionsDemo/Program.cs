0976744f3bbb0712b84c09a657e0ab96ea0fca9f
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-08-07 15:55:33 -0400

    Day 3 - collections and exceptions

---
+++ Day 3 - Collections, Exceptions, OOPS/collectionsDemo/Program.cs
@@ -0,0 +1,186 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+
+#region Sample code to show values gets overridden with data types
+//Console.WriteLine("Hello, World!");
+
+
+// string guestName = "Alex";
+// // Console.WriteLine(guestName);
+
+// guestName = "Maria";
+// Console.WriteLine(guestName);
+
+// guestName = "John"; //this will overide the previous value
+#endregion
+
+#region Array Demo
+
+#region example 1
+// string[] friends = new string[5];
+// friends[0] = "Alex";
+// friends[1] = "Maria";
+// friends[2] = "Smith";
+// friends[3] = "Stephen";
+// friends[4] = "Shailesh";
+
+// Console.WriteLine(friends[0]);
+// Console.WriteLine(friends[1]);
+// Console.WriteLine(friends[2]);
+// Console.WriteLine(friends[3]);
+// Console.WriteLine(friends[4]);
+#endregion
+
+#region  example 2
+
+// Console.WriteLine("How many friends do you have ?" );
+// int friendsCount = int.Parse(Console.ReadLine());
+
+// string[] friends = new string[friendsCount];
+
+// for(int i=0; i < friendsCount; i++)
+// {
+//     Console.WriteLine("Enter friend name " + (i+1)); 
+//     friends[i] = Console.ReadLine();
+//     Console.Clear();
+// }
+
+// Console.Clear();
+// for(int i = 0 ;i<friendsCount; i++)
+// {
+//     Console.WriteLine(friends[i]);
+// }
+#endregion
+
+#region example 3
+
+// int[] favNumber = new int[10];
+
+// int sumOfNumber = 0;
+
+
+
+// for (int i = 0; i < favNumber.Length; i++)
+// {
+//     Console.WriteLine("Enter your favourite number : ");
+//     favNumber[i] = int.Parse(Console.ReadLine());
+//     Console.Clear();
+// }
+
+
+
+// for (int i = 0; i < favNumber.Length ; i++)
+// {
+//     sumOfNumber = sumOfNumber + favNumber[i];
+// }
+
+// Console.WriteLine("Sum of all your numbers are  " + sumOfNumber);
+
+
+
+
+#endregion
+
+#endregion
+
+
+#region ArrayList Demo
+#region example 1
+// ArrayList myValues = new ArrayList();
+
+// myValues.Add(10);
+// myValues.Add("Nikhil");
+// myValues.Add(20.30);
+// myValues.Add(true);
+// myValues.Add("Julie");
+// myValues.Add(-40);
+
+
+// for(int i = 0; i < myValues.Count; i++)
+// {
+//     Console.WriteLine(myValues[i]);
+// }
+#endregion
+
+#region example 2
+// ArrayList mynewvalues = new ArrayList();
+
+// string val = "";
+
+// while (val != "End")
+// {
+// Console.WriteLine("Enter the value, keep entering till you want, Type End to exit");
+// val = Console.ReadLine();
+//     if(val != "End")
+//     {
+//         mynewvalues.Add(val);
+//     }
+// }
+
+// Console.Clear();
+// Console.WriteLine("Thank you");
+#endregion
+
+#endregion
+
+
+#region List
+
+
+
+// List<string> friends = new List<string>();
+// //keep on adding the values
+// friends.Add("Alex");
+// friends.Add("Maria");
+// friends.Add("John");
+// friends.Add("Stephen");
+// friends.Add("Shailesh");
+// friends.Add("Sonam");	
+// friends.Add("Marie");
+// friends.Add("Peter");
+// friends.Add("Parker");
+// friends.Add("Jane");
+// friends.Add("Drew Berry");
+// friends.Add("John Cena");
+
+// foreach(var item in friends)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+// System.Console.WriteLine("Total friends " + friends.Count);
+
+#endregion
+
+
+//dictionary is another collection which is for key and a value pair
+
+
+
+Dictionary<int,string> guestList = new Dictionary<int, string>();
+
+//int here is a key - which will need to be unique throughout the collection
+//string is the value - which will always be string in this case
+
+guestList.Add(101,"Nikhil");
+guestList.Add(102,"Alex");
+guestList.Add(103,"Maria");
+guestList.Add(104,"John");
+guestList.Add(105,"Stephen");
+guestList.Add(106,"Shailesh");
+guestList.Add(107,"Sonam");
+guestList.Add(108,"Marie");
+guestList.Add(109,"Peter");
+guestList.Add(110,"Parker");
+guestList.Add(111,"Jane");
+guestList.Add(112,"Drew Berry");
+guestList.Add(113,"John Cena");
+guestList.Add(114,"Julie"); 
+
+foreach(var item in guestList)
+{   
+    System.Console.WriteLine(item.Key + " " + item.Value);
+}
+
---
+++ Day 3 - Collections, Exceptions, OOPS/exceptionHandling_demo/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region example 1 - Handle System Exception
+
+// try
+// {
+// Console.WriteLine("Enter First  number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int divideResult = num1 / num2;
+// Console.WriteLine(divideResult);
+
+// }
+// catch(FormatException ex)
+// {
+//     Console.WriteLine("Please Enter number in Numeric format only");
+// }
+// catch(DivideByZeroException ex)
+// {
+//     Console.WriteLine("You cannot divide any number by Zero universally");
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine(ex.Message);
+// }
+
+
+// Console.WriteLine("Thank you for useing my program");
+#endregion
+
+
+bool continueExecution = true;
+
+while(continueExecution)
+{
+try
+{
+    List<string> friends = new List<string>();
+    string val = "";	
+
+        while(val.Length != 1)
+        {
+            Console.WriteLine("Enter your friends name");
+           val = Console.ReadLine();
+        }
+
+    // while(val != "End")
+    // {
+    //       Console.WriteLine("Enter your friends name");
+    //       val = Console.ReadLine();
+    //       if(val.Length == 1)
+    //       {
+    //           throw new Exception("Your name should not be less than 2 characters " + val + " is not added to friends list ");
+    //       }
+    //       else
+    //       {
+
+    //       friends.Add(val);
+    //       if(val == "End")
+    //       {
+    //         continueExecution = false;
+    //         Console.WriteLine("Thank you for using my program, all the friends are added and save");
+    //       }
+    //       }
+    // }
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    Console.WriteLine("Thank you for using my program");
+}
+}
